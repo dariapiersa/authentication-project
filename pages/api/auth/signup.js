b4fed4ddb760761dc9e809fafde4d7503de92954
Daria Piersa <dariapiersa17@gmail.com>
2022-10-18 12:45:02 +0200

    sending signup requests from the frontend & unique email address

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,6 +2,9 @@ import { hashPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
 
 async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
   const data = req.body;
 
   const { email, password } = data;
@@ -12,26 +15,32 @@ async function handler(req, res) {
     !password ||
     password.trim().length < 7
   ) {
-    res
-      .status(422)
-      .json({
-        message:
-          "Invalid input - password should also be at least 7 characters long.",
-      });
+    res.status(422).json({
+      message:
+        "Invalid input - password should also be at least 7 characters long.",
+    });
     return;
   }
 
   const client = await connectToDatabase();
 
   const db = client.db();
-  const hashedPassword = hashPassword(password);
+
+  const existingUser = await db.collection('users').findOne({email: email});
+  if(existingUser){
+    res.status(422).json({message: 'User exists already!'});
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(password);
 
   const result = await db.collection("users").insertOne({
     email: email,
-    password: hashedPassword
+    password: hashedPassword,
   });
 
-  res.status(201).json({message: 'Created user!'});
+  res.status(201).json({ message: "Created user!" });
 }
 
 export default handler;
